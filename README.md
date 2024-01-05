9d2086dcd4469aa7becdf6f6820771b6c807cb4d
StehChoma <105441843+StehChoma@users.noreply.github.com>
2024-01-05 20:42:38 +0000

    adiciona scripts 'lint:check'e 'lint:fix'

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto pra treinar programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Eu te amo! mais que sushi</h1>
+  return <h1>Eu te amo! mais que sushi</h1>;
 }
 
-export default Home
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
+
+export default Home;
