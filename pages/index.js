6a3f3eb68c15278147b0b8fa44247470d2b24f8f
StehChoma <105441843+StehChoma@users.noreply.github.com>
2024-01-16 20:40:25 +0000

    install

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Eu te amo! mais que sushi</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
